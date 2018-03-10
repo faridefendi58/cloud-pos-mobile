2aaee1ced6ebbbfc2debfc5af47e3a798e691374
Farid Efendi <miftahfaridse@gmail.com>
2018-03-10 10:05:23 +0700

    Main login page

---
+++ app/src/main/java/com/slightsite/ucokinventory/Server.java
@@ -0,0 +1,9 @@
+package com.slightsite.ucokinventory;
+
+/**
+ * Created by mrsvette on 09/03/18.
+ */
+
+public class Server {
+    public static final String URL = "http://pos.slightsite.com/api/";
+}
