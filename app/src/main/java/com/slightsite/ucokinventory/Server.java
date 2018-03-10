1cb369dbc0dad0cdacfe61a62fd6a15dbe32fa0c
Farid Efendi <miftahfaridse@gmail.com>
2018-03-10 10:13:28 +0700

    Avoiding useless files

--- app/src/main/java/com/slightsite/ucokinventory/Server.java
+++
@@ -1,9 +0,0 @@
-package com.slightsite.ucokinventory;
-
-/**
- * Created by mrsvette on 09/03/18.
- */
-
-public class Server {
-    public static final String URL = "http://pos.slightsite.com/api/";
-}
