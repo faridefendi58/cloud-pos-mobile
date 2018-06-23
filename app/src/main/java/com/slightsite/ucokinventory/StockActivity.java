d047b6e429c931ada2776ad4fea03c8d7f437305
Farid Efendi <33080462+faridefendi58@users.noreply.github.com>
2018-06-23 17:12:58 +0700

    List stock on each wh (#21)
    * List stock on each wh
    
    * Stock activity title

--- app/src/main/java/com/slightsite/ucokinventory/StockActivity.java
+++ app/src/main/java/com/slightsite/ucokinventory/StockActivity.java
@@ -3,15 +3,27 @@ package com.slightsite.ucokinventory;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,6 +54,21 @@ public class StockActivity extends MainActivity {
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
 
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private StockActivity.SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,17 +77,25 @@ public class StockActivity extends MainActivity {
         setDinamicContent(R.layout.app_bar_stock);
         buildMenu();
 
-        // build spinner wh list
-        Spinner step1_wh_list = (Spinner)findViewById(R.id.step1_wh_list);
-        ArrayAdapter<String> whAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, get_list_warehouse());
-        whAdapter.notifyDataSetChanged();
-        step1_wh_list.setAdapter(whAdapter);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        final Context ini = this;
-        Button btn_next = (Button) findViewById(R.id.btn_next);
-        trigger_btn_step1(btn_next, ini);
-        Button btn_back = (Button) findViewById(R.id.btn_back);
-        trigger_btn_back(btn_back, ini);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // delay build the form after tabs fully finished
+                buildTheStockList();
+            }
+        }, 1000);
     }
 
     @Override
@@ -168,47 +203,75 @@ public class StockActivity extends MainActivity {
         return items;
     }
 
-    private void trigger_btn_step1(Button btn, final Context ini) {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // hide step1
-                LinearLayout step1 = (LinearLayout) findViewById(R.id.step1);
-                step1.setVisibility(View.GONE);
-                // show step2
-                LinearLayout step2 = (LinearLayout) findViewById(R.id.step2);
-                step2.setVisibility(View.VISIBLE);
-                // set the header label
-                Spinner step1_wh_list = (Spinner)findViewById(R.id.step1_wh_list);
-                TextView textView2 = (TextView) findViewById(R.id.TextView2);
-                textView2.setText(step1_wh_list.getSelectedItem().toString());
-                // show the list
-                set_list_stock(ini);
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            switch (position) {
+                case 0:
+                    TabFragment1 tab1 = new TabFragment1();
+                    return tab1;
+                case 1:
+                    TabFragment2 tab2 = new TabFragment2();
+                    return tab2;
+                default:
+                    return null;
             }
-        });
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 1;
+        }
     }
 
-    private void trigger_btn_back(Button btn, final Context ini) {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // hide step1
-                LinearLayout step1 = (LinearLayout) findViewById(R.id.step1);
-                step1.setVisibility(View.VISIBLE);
-                // show step2
-                LinearLayout step2 = (LinearLayout) findViewById(R.id.step2);
-                step2.setVisibility(View.GONE);
-            }
-        });
+    public static class TabFragment1 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.tab_fragment_stock_1, container, false);
+        }
     }
 
-    private ArrayList set_list_stock(final Context ini) {
-        Spinner step1_wh_list = (Spinner)findViewById(R.id.step1_wh_list);
-        String wh_name = step1_wh_list.getSelectedItem().toString();
+    public static class TabFragment2 extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.tab_fragment_stock_2, container, false);
+        }
+    }
+
+    private void buildTheStockList()
+    {
+        /*ArrayList get_list_warehouse = get_list_warehouse();
+        Log.e(TAG, "Size : "+ get_list_warehouse.size());
+
+        TableLayout table_layout = (TableLayout) findViewById(R.id.table_layout);
+
+
+        for (int i = 0; i < get_list_warehouse.size(); i++) {
+            TableRow row = new TableRow(this);
+            //row.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+            TextView wh = new TextView(this);
+            wh.setText("cuk "+ i);
+
+            table_layout.addView(row);
+            row.addView(wh);
+        }*/
+        final TableLayout table_layout = (TableLayout) findViewById(R.id.table_layout);
+        final Context ini = this;
+
         Map<String, String> params = new HashMap<String, String>();
-        params.put("warehouse_name", wh_name);
-        //params.put("warehouse_id", "1");
-        Log.e(TAG, "Response: " + params.toString());
 
         final ArrayList<String> items = new ArrayList<String>();
 
@@ -217,26 +280,89 @@ public class StockActivity extends MainActivity {
                 new StockActivity.VolleyCallback() {
                     @Override
                     public void onSuccess(String result) {
-                        Log.e(TAG, "Response: " + result.toString());
                         try {
                             JSONObject jObj = new JSONObject(result);
                             success = jObj.getInt(TAG_SUCCESS);
                             // Check for error node in json
                             if (success == 1) {
-                                JSONArray data = jObj.getJSONArray("data");
-                                Log.e(TAG, "Response: " + data.toString());
-                                for(int n = 0; n < data.length(); n++)
-                                {
-                                    JSONObject data_n = data.getJSONObject(n);
-                                    String item = data_n.getString("product_name")+" : "
-                                            + data_n.getString("quantity") + " " + data_n.getString("unit");
-                                    items.add( item );
-                                }
-                                ArrayAdapter adapter = new ArrayAdapter<String>(ini,
-                                        R.layout.activity_list_view, items);
+                                JSONObject data = jObj.getJSONObject("data");
+                                Iterator<String> iter = data.keys();
+                                Integer idx = 0;
+                                while (iter.hasNext()) {
+                                    String key = iter.next();
+                                    try {
+                                        String DK = "0";
+                                        String DD = "0";
+                                        String PC = "0";
+                                        String DC = "0";
+                                        if ( data.get(key) instanceof JSONObject ) {
+                                            JSONObject iterate_data = new JSONObject(data.get(key).toString());
+                                            JSONObject wh_data = new JSONObject(iterate_data.getString("wh_data"));
+                                            if (iterate_data.getString("stock_data").length() > 2) {
+                                                JSONArray stock_data = iterate_data.getJSONArray("stock_data");
+                                                //Log.e(TAG, "Stock data : "+ stock_data.toString());
+                                                for(int n = 0; n < stock_data.length(); n++)
+                                                {
+                                                    JSONObject stock_data_n = stock_data.getJSONObject(n);
+                                                    if (stock_data_n.getString("code").equals("DK")) {
+                                                        DK = stock_data_n.getString("quantity");
+                                                    }
+                                                    if (stock_data_n.getString("code").equals("DD")) {
+                                                        DD = stock_data_n.getString("quantity");
+                                                    }
+                                                    if (stock_data_n.getString("code").equals("PC")) {
+                                                        PC = stock_data_n.getString("quantity");
+                                                    }
+                                                    if (stock_data_n.getString("code").equals("DC")) {
+                                                        DC = stock_data_n.getString("quantity");
+                                                    }
+                                                }
+                                            }
 
-                                ListView listView = (ListView) findViewById(R.id.list);
-                                listView.setAdapter(adapter);
+                                            TableRow row = new TableRow(ini);
+                                            TextView wh = new TextView(ini);
+                                            wh.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                                                    TableRow.LayoutParams.WRAP_CONTENT));
+
+                                            wh.setGravity(Gravity.LEFT);
+                                            wh.setPadding(5, 15, 0, 15);
+
+                                            wh.setText(wh_data.getString("title"));
+
+                                            //table_layout.addView(row);
+                                            row.addView(wh);
+
+                                            for(int n = 0; n < 4; n++)
+                                            {
+                                                TextView tv1 = new TextView(ini);
+                                                tv1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+
+                                                tv1.setGravity(Gravity.LEFT);
+                                                tv1.setPadding(0, 15, 0, 15);
+
+                                                if (n == 0) {
+                                                    tv1.setText(DK);
+                                                } else if (n == 1) {
+                                                    tv1.setText(DD);
+                                                } else if (n == 2) {
+                                                    tv1.setText(PC);
+                                                } else if (n == 3) {
+                                                    tv1.setText(DC);
+                                                }
+
+                                                row.addView(tv1);
+                                            }
+                                            idx ++;
+                                            if ((idx % 2) == 0) {
+                                                row.setBackgroundColor(Color.parseColor("#ebebeb"));
+                                            }
+                                            table_layout.addView(row);
+                                        }
+                                    } catch (JSONException e) {
+                                        // Something went wrong!
+                                        e.printStackTrace();
+                                    }
+                                }
                             }
 
                         } catch (JSONException e) {
@@ -244,7 +370,5 @@ public class StockActivity extends MainActivity {
                         }
                     }
                 });
-
-        return items;
     }
 }
